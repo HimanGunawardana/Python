145465662349bfb1901160c8c65c023e8f82ff7e
Himan Henuka Gunawardana <himanhenuka18@gmail.com>
2023-11-20 10:37:23 +0530

    20.11.2023 10.37a.m.

---
+++ Python TSD/Foor loop 8.py
@@ -0,0 +1,8 @@
+value =  ['U','N','I','V','E','R','S','I','T','Y']
+print(value[2])
+print(value[2:5])
+print(value[-1])
+print(value[2:10:2])
+print(value[:])
+print(value[::-1])
+print(value[::2])
