e5e3171f007d67080f7d4743a28cf5a70186d71e
Himan Henuka Gunawardana <himanhenuka18@gmail.com>
2023-12-12 22:50:29 +0530

    update

---
+++ Python File/ABC1.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+pensize(5)
+color("purple")
+forward(200)
+color("orange")
+right(90)
+forward(200)
+color("yellow")
+right(90)
+forward(200)
+
+right(90)
+color("red")
+forward(200)
